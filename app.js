5db58095257d224b9d921a8de82d2d537e848141
Anuj Rao <anujr@tekion.com>
2023-06-07 18:37:40 +0530

    Some intro for globals in nodejs

---
+++ app.js
@@ -0,0 +1,17 @@
+
+/**
+some globals provided by nodejs 
+  __dirname   -> path to current directory
+  __filename  -> path to current file name
+  require     -> function to use modules
+  module      -> info about current module (file)
+  process     -> info about env where program is executed
+  setTimeout
+  setInterval
+these are just some gloabls, there are a lot more
+
+*/
+
+console.log(__dirname)
+console.log(__filename)
+console.log(module)
