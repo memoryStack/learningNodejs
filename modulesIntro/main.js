95d202032553fba090f151699a8289342d4f65b6
Anuj Rao <anujr@tekion.com>
2023-06-07 18:57:30 +0530

    Add intro on breaking code in separate files and using exports and require

---
+++ modulesIntro/main.js
@@ -0,0 +1,7 @@
+
+const Names = require('./names')
+const sayHi = require('./util')
+
+sayHi(Names.Anuj)
+sayHi(Names.Rohit)
+sayHi(Names.Ram)
---
+++ modulesIntro/names.js
@@ -0,0 +1,6 @@
+
+const Anuj = 'Anuj'
+const Rohit = 'Rohit'
+const Ram = 'Ram'
+
+module.exports = { Anuj, Rohit, Ram }
