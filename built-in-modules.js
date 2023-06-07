6e8fa245b8d4d3d6b2cfd2a6f98a398bee80f19f
Anuj Rao <anujr@tekion.com>
2023-06-07 19:41:11 +0530

    Small intro about built-in modules

---
+++ built-in-modules.js
@@ -0,0 +1,12 @@
+
+
+/*
+these are some of the built-in modules in nodejs
+for some specific functionalities. these will be explored later.
+  os
+  path
+  http
+  fs
+
+there are a lot more built-in modules but these are major and will be more used for now.
+*/
