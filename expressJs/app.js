ec53112247f05cfe169a4b806d25e7d760fe32c4
Anuj Rao <anujr@tekion.com>
2023-06-09 09:49:35 +0530

    Add another way of serving index.html file

--- expressJs/app.js
+++ expressJs/app.js
@@ -4,9 +4,9 @@ const app = express()
 
 app.use(express.static('./public'))
 
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './public/index.html'))
-})
+// app.get('/', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, './public/index.html'))
+// })
 
 // NOTE: put it below the other handlers
 // else it will be called for all the incoming requests
